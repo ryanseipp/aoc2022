bec1daf09a5a299624cf880aa1edfd217d616344
Ryan Seipp <ryan@seipp.us>
2022-12-02 14:04:17 -0500

    Add imperative version to benchmark difference

--- day_one/benches/day_one.rs
+++ day_one/benches/day_one.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use day_one::{part_one, part_two};
+use day_one::{part_one, part_one_imperative, part_two, part_two_imperative};
 
 fn benchmark(c: &mut Criterion) {
     let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
@@ -10,9 +10,19 @@ fn benchmark(c: &mut Criterion) {
     group.bench_with_input(BenchmarkId::new("part_one", "iterator"), &input, |b, i| {
         b.iter(|| part_one(i))
     });
+    group.bench_with_input(
+        BenchmarkId::new("part_one", "imperative"),
+        &input,
+        |b, i| b.iter(|| part_one_imperative(i)),
+    );
     group.bench_with_input(BenchmarkId::new("part_two", "iterator"), &input, |b, i| {
         b.iter(|| part_two(i))
     });
+    group.bench_with_input(
+        BenchmarkId::new("part_two", "imperative"),
+        &input,
+        |b, i| b.iter(|| part_two_imperative(i)),
+    );
 }
 
 criterion_group!(benches, benchmark);
--- day_one/src/lib.rs
+++ day_one/src/lib.rs
@@ -7,6 +7,26 @@ pub fn part_one(input: &str) -> i32 {
         .unwrap();
 }
 
+pub fn part_one_imperative(input: &str) -> i32 {
+    let contents: Vec<&str> = input.split('\n').collect();
+
+    let mut max_calories: i32 = 0;
+    let mut elf_sum: i32 = 0;
+
+    for line in contents {
+        if line == "" {
+            if elf_sum > max_calories {
+                max_calories = elf_sum;
+            }
+            elf_sum = 0;
+        } else {
+            elf_sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    return max_calories;
+}
+
 pub fn part_two(input: &str) -> i32 {
     let contents: Vec<&str> = input.split('\n').collect();
     let mut sums: Vec<i32> = contents
@@ -14,8 +34,40 @@ pub fn part_two(input: &str) -> i32 {
         .map(|elf| elf.iter().map(|x| x.parse::<i32>().unwrap()).sum::<i32>())
         .collect();
 
-    sums.sort_by(|a, b| b.cmp(a));
-    return sums.iter().take(3).sum();
+    sums.sort();
+    return sums.iter().rev().take(3).sum();
+}
+
+pub fn part_two_imperative(input: &str) -> i32 {
+    let contents: Vec<&str> = input.split('\n').collect();
+
+    let mut top_three_calories: [i32; 3] = [0; 3];
+    let mut elf_sum: i32 = 0;
+
+    for line in contents {
+        if line == "" {
+            let mut shift_right = false;
+            let mut prev_value: i32 = 0;
+
+            for index in 0..top_three_calories.len() {
+                if shift_right {
+                    let temp = top_three_calories[index];
+                    top_three_calories[index] = prev_value;
+                    prev_value = temp;
+                } else if elf_sum > top_three_calories[index] {
+                    shift_right = true;
+                    prev_value = top_three_calories[index];
+                    top_three_calories[index] = elf_sum;
+                }
+            }
+
+            elf_sum = 0;
+        } else {
+            elf_sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    return top_three_calories.iter().sum();
 }
 
 #[cfg(test)]
@@ -31,8 +83,20 @@ mod test {
     }
 
     #[test]
+    fn part_one_imperative_returns_correct_result() {
+        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
+        assert_eq!(71924, part_one_imperative(input.as_str()));
+    }
+
+    #[test]
     fn part_two_returns_correct_result() {
         let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
         assert_eq!(210406, part_two(input.as_str()));
+    }
+
+    #[test]
+    fn part_two_imperative_returns_correct_result() {
+        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
+        assert_eq!(210406, part_two_imperative(input.as_str()));
     }
 }
