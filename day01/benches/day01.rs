899696aacf0e6d9a64436d6afed637a0bfa0391b
Ryan Seipp <ryan@seipp.us>
2022-12-13 15:46:03 -0500

    Optimize day01 iterating by bytes

--- day01/benches/day01.rs
+++ day01/benches/day01.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use day01::{part_one, part_one_imperative, part_two, part_two_imperative};
+use day01::*;
 
 fn benchmark(c: &mut Criterion) {
-    let input = fs::read_to_string("day01/input.txt").expect("File input.txt should exist");
+    let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
 
     let mut group = c.benchmark_group("day_one");
     group.bench_with_input(BenchmarkId::new("part_one", "iterator"), &input, |b, i| {
@@ -15,6 +15,9 @@ fn benchmark(c: &mut Criterion) {
         &input,
         |b, i| b.iter(|| part_one_imperative(i)),
     );
+    group.bench_with_input(BenchmarkId::new("part_one", "bitwise"), &input, |b, i| {
+        b.iter(|| part_one_bitwise(i))
+    });
     group.bench_with_input(BenchmarkId::new("part_two", "iterator"), &input, |b, i| {
         b.iter(|| part_two(i))
     });
@@ -23,6 +26,9 @@ fn benchmark(c: &mut Criterion) {
         &input,
         |b, i| b.iter(|| part_two_imperative(i)),
     );
+    group.bench_with_input(BenchmarkId::new("part_two", "bitwise"), &input, |b, i| {
+        b.iter(|| part_two_bitwise(i))
+    });
 }
 
 criterion_group!(benches, benchmark);
--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -27,6 +27,30 @@ pub fn part_one_imperative(input: &str) -> i32 {
     return max_calories;
 }
 
+pub fn part_one_bitwise(input: &str) -> i32 {
+    let bytes = input.as_bytes();
+    let mut max: i32 = 0;
+    let mut sum: i32 = 0;
+    let mut current: i32 = 0;
+
+    for byte in bytes {
+        if *byte != b'\n' {
+            current *= 10;
+            current += (byte - b'0') as i32;
+        } else if current == 0 && sum > max {
+            max = sum;
+            sum = 0;
+        } else if current == 0 {
+            sum = 0;
+        } else {
+            sum += current;
+            current = 0;
+        }
+    }
+
+    return max;
+}
+
 pub fn part_two(input: &str) -> i32 {
     let contents: Vec<&str> = input.split('\n').collect();
     let mut sums: Vec<i32> = contents
@@ -70,33 +94,79 @@ pub fn part_two_imperative(input: &str) -> i32 {
     return top_three_calories.iter().sum();
 }
 
+pub fn part_two_bitwise(input: &str) -> i32 {
+    let bytes = input.as_bytes();
+    let mut top_three_calories: [i32; 3] = [0; 3];
+    let mut sum: i32 = 0;
+    let mut current: i32 = 0;
+
+    for byte in bytes {
+        if *byte != b'\n' {
+            current *= 10;
+            current += (byte - b'0') as i32;
+        } else if current == 0 {
+            let mut shift_right = false;
+            let mut prev_value: i32 = 0;
+
+            for index in 0..top_three_calories.len() {
+                if shift_right {
+                    let temp = top_three_calories[index];
+                    top_three_calories[index] = prev_value;
+                    prev_value = temp;
+                } else if sum > top_three_calories[index] {
+                    shift_right = true;
+                    prev_value = top_three_calories[index];
+                    top_three_calories[index] = sum;
+                }
+            }
+
+            sum = 0;
+        } else {
+            sum += current;
+            current = 0;
+        }
+    }
+
+    return top_three_calories[0] + top_three_calories[1] + top_three_calories[2];
+}
+
 #[cfg(test)]
 mod test {
     use std::fs;
 
     use super::*;
 
+    fn get_input() -> String {
+        return fs::read_to_string("input.txt").expect("File input.txt should exist");
+    }
+
     #[test]
     fn part_one_returns_correct_result() {
-        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
-        assert_eq!(71924, part_one(input.as_str()));
+        assert_eq!(71924, part_one(&get_input()));
     }
 
     #[test]
     fn part_one_imperative_returns_correct_result() {
-        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
-        assert_eq!(71924, part_one_imperative(input.as_str()));
+        assert_eq!(71924, part_one_imperative(&get_input()));
+    }
+
+    #[test]
+    fn part_one_bitwise_returns_correct_result() {
+        assert_eq!(71924, part_one_bitwise(&get_input()));
     }
 
     #[test]
     fn part_two_returns_correct_result() {
-        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
-        assert_eq!(210406, part_two(input.as_str()));
+        assert_eq!(210406, part_two(&get_input()));
     }
 
     #[test]
     fn part_two_imperative_returns_correct_result() {
-        let input = fs::read_to_string("input.txt").expect("File input.txt should exist");
-        assert_eq!(210406, part_two_imperative(input.as_str()));
+        assert_eq!(210406, part_two_imperative(&get_input()));
+    }
+
+    #[test]
+    fn part_two_bitwise_returns_correct_result() {
+        assert_eq!(210406, part_two_bitwise(&get_input()));
     }
 }
